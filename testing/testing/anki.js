6e815bd78c03fa7ac888b7517a0a461afa6c4064
cheek-codes <cheek.codes@gmail.com>
2023-03-10 22:01:40 -0500

    testing

---
+++ testing/testing/anki.js
@@ -0,0 +1,7 @@
+let isNightTime = true
+
+if(isNightTime){
+    console.log("turn on the lights!")
+}else{
+    console.log("turn off the lights!")
+}
--- testing/testing/testing.js
+++ testing/testing/testing.js
@@ -1,116 +1,50 @@
-//come up with a parent class
-//extend that arent class into 2 children
-//use encapsulation, abstraction, inheritance and polymorphism
+// 1. this is what is was doing
+// 2. i didn't like doing it
+// 3. here's how i tried to fix it
+// 3. started freelancing
+// 4. and now im here
 
-class Contractor{
-    constructor(name, role){
-        this._name = name
-        this._role = role
-    }
-    get name(){
-        return this._name
-    }
-    get role(){
-        return this._role
-    }
-    introduce(){
-        console.log(`Hi, my name is ${this._name} and I'm a ${this._role} developer`)
-    }
+/* 
+- was always interested in coding.
+- started with myspace.
+- went to medical school.
+- used coding to help me study better.
+- hated studying with paper and pencil and reading so i used anki and customized my study notes and cards. I made it beautiful, and fun.
+- after a while, i started to notice the abuse medical staff have to endure everyday from patients, family of patients, the hazing that is considered normal. I also didn't like how about half the time, we are awake for 48 hours, sometimes 72 hours at a time. I didn't feel right taking care of patients after staying awake for that long. yet we tell patients sleep is important.
+- i decided to take a break from medical school. 
+- during the break, I started freelancing.
+*/
+
+
+
+
+
+//100 hours project description. have simple using wireframe.cc, figma, or balsamiq or just draw it out
+
+
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value
+  console.log(choice)
+  const url = `https://openlibrary.org/isbn/${choice}.json`
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data.title)
+        if(!localStorage.getItem('books')){
+            localStorage.setItem('books', data.title)
+        }else{
+           let books = localStorage.getItem('books') + " ; " + data.title
+           localStorage.setItem('books', books)
+        }
+          //put title into localStorage
+          // let books = localStorage.getItem('books') + " ; " + data.title
+          // localStorage.setItem('books', books)
+        document.querySelector('h2').innerText = localStorage.getItem('books')
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
 }
-
-class Front extends Contractor{
-    constructor(name, role, tech){
-        super(name, role)
-        this._tech = tech
-    }
-    get tech(){
-        return this._tech
-    }
-    introduce(){
-        super.introduce()
-        console.log(`I work with ${this._tech}`)
-    }
-}
-
-class Back extends Front{
-    constructor(name, role, tech){
-        super(name, role, tech)
-    }
-    introduce(){
-        super.introduce()
-    }
-}
-
-let sandy = new Back("Sandy", "back-end", "node")
-
-let liam = new Front("liam", "front-end", "react")
-
-let agency = [sandy, liam]
-
-for (person of agency){
-    person.introduce()
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// class Contractor{
-//     constructor(name, role){
-//         this._name = name
-//         this._role = role
-//     }
-//     get name(){
-//         return this._name
-//     }
-//     get role(){
-//         return this._role
-//     }
-//     sayHello(){
-//         console.log(`Hello, I am on the ${this._role} team at #100Devs!`)
-//     }
-// }
-
-// class Front extends Contractor{
-//     constructor(name, role, tech){
-//         super(name, role)
-//         this._tech = tech
-//     }
-//     get tech(){
-//         return this._tech
-//     }
-//     sayHello(){
-//         console.log(`Hello, I am on the ${this._role} team at #100Devs! And I use ${this._tech}`)
-//     }
-// }
-
-// class Back extends Contractor{
-//     constructor(name, role, tech){
-//         super(name, role)
-//         this._tech =tech
-//     }
-//     sayHello(){
-//         console.log(`Hello, I am on the ${this._role} team at #100Devs! And I use ${this._tech}`)
-//     }
-// }
-
-// let bob = new Contractor("Bob", "front-end")
-// let simba = new Front ("simba", "front-end", "react")
-// let machi = new Back ("machi", "back-end", "node")
-
-// let agency = [bob, simba, machi]
-
-// for(person of agency){
-//     person.sayHello()
-// }
