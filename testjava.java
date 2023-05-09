ef3bfa7eec2a39154be9461402e6d50ad3f8e031
cheek-codes <cheek.codes@gmail.com>
2023-05-09 13:25:19 -0400

    java tut

---
+++ testjava.java
@@ -0,0 +1,15 @@
+public class testjava {
+    public static void main(String[] args) {
+        System.out.println("I love pizza");
+        System.out.print("It's really good!");
+        System.out.println();
+    }
+}
+
+/**
+ * Innertestjava
+ */
+public class Innertestjava {
+
+    
+}
