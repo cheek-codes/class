9f32aa4be487ba63038fb10c0b5d25b178b98fcf
cheek-codes <cheek.codes@gmail.com>
2023-05-14 13:55:51 -0400

    java

--- testjava.java
+++ testjava.java
@@ -1,15 +1,19 @@
 public class testjava {
     public static void main(String[] args) {
-        System.out.println("I love pizza");
-        System.out.print("It's really good!");
-        System.out.println();
+
+        String koolaid = "koolaid";
+        String water = "water";
+        String emptyGlass;
+
+        System.out.println("koolaid glass has " + koolaid);
+        System.out.println("water glass has " + water);
+
+        emptyGlass = koolaid;
+        // koolaid = water;
+        // water = emptyGlass;
+
+        System.out.println("koolaid glass now has " + koolaid);
+        // System.out.println("water glass now has " + water);
+        // System.out.println("empty glass now has " + emptyGlass);
     }
-}
-
-/**
- * Innertestjava
- */
-public class Innertestjava {
-
-    
 }
